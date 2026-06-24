656a858264425a01e8d2a2f1470ae737d18bba2f
kohei.saito <fu.saito.nf.k@gmail.com>
2026-06-25 01:33:49 +0900

    誤削除したメソッドの復元

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class Announcement extends Model
@@ -73,6 +74,42 @@ class Announcement extends Model
     }
 
     /**
+     * 公開中のお知らせを公開ステータスと既読数を含めて取得
+     *
+     * @param  string|int  $user_id ユーザーID
+     * @return array{ unread_count: int, announcement: array|string }
+     */
+    public function getStatusRead(string|int $user_id): array
+    {
+        // 公開中のお知らせIDを取得
+        $announcement_ids = $this->getPublicAnnouncements(true);
+
+        if (!empty($announcement_ids)) {
+            $m_announcement_read = new AnnouncementRead();
+            $read_info           = $m_announcement_read->getCount($user_id, $announcement_ids);
+            $read_count          = data_get($read_info, 'read_count', 0);
+            $announcement_list   = [
+                'unread_count' => count($announcement_ids) - $read_count,
+                'announcement' => $this->getPublicAnnouncements(),
+            ];
+            // 既読のお知らせに pub_status を付与する（未読はキー自体作成しない）
+            foreach ($announcement_list['announcement'] as $a_key => $a_val) {
+                foreach (data_get($read_info, 'id', []) as $r_id) {
+                    if ($a_val->id === data_get($r_id, 'announcement_id')) {
+                        $announcement_list['announcement'][$a_key]->pub_status = 1;
+                    }
+                }
+            }
+            return $announcement_list;
+        }
+
+        return [
+            'unread_count' => 0,
+            'announcement' => '',
+        ];
+    }
+
+    /**
      * 公開中のお知らせのみを取得
      *
      * @param bool $only_id IDのみを取得する場合true
@@ -89,17 +126,16 @@ class Announcement extends Model
             ->whereNull('deleted_at')
             ->get()
             ->toArray();
-        // 取得結果の中に公開終了しているお知らせがあったら削除
+        // 取得結果の中に公開終了しているお知らせがあったら除外
         foreach ($tmp as $val) {
             if (!empty(data_get($val, 'pub_end_at')) && $now > data_get($val, 'pub_end_at', '')) {
                 continue;
+            }
+            if ($only_id === true) {
+                /* IDのみの配列になるようにする */
+                $res[] = data_get($val, 'id');
             } else {
-                if ($only_id === true) {
-                    /* IDのみの配列になるようにする */
-                    array_push($res, data_get($val, 'id'));
-                } else {
-                    $res[] = $val;
-                }
+                $res[] = $val;
             }
         }
 
